0ad3f3913a3d365ed5212f4e54869794d39cd984
Anton Babenko <anton@antonbabenko.com>
2017-05-12 13:50:10 +0200

    something

--- terraform/demo1/main.tf
+++ terraform/demo1/main.tf
@@ -1,7 +1,34 @@
 # Note: Environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY should be set before running this.
+//export AWS_ACCESS_KEY_ID=...
+//export AWS_SECRET_ACCESS_KEY=...
+//export AWS_DEFAULT_REGION=eu-west-1
+//aws s3 ls
 
 terraform {
   required_version = ">=0.9"
+
+ backend "s3" {
+   bucket     = "my-tf-states-anton-demo"
+   key        = "user6"
+   region     = "eu-west-1"
+   lock_table = "terraform_locks"
+   encrypt    = true
+ }
+}
+
+data "terraform_remote_state" "user2" {
+ backend = "s3"
+
+ config {
+   bucket  = "my-tf-states-anton-demo"
+   region  = "eu-west-1"
+   key     = "user2"
+   encrypt = true
+ }
+}
+
+output "user2_s3_bucket_test_arn" {
+ value = "${data.terraform_remote_state.user2.s3_bucket_test_arn}"
 }
 
 
@@ -28,9 +55,11 @@ resource "random_pet" "bucket" {
   length = 2
 }
 
-data "aws_caller_identity" "this" {}
-
 
 output "s3_bucket_test_arn" {
   value = "${aws_s3_bucket.test.arn}"
+}
+
+output "s3_bucket_test_bucket" {
+  value = "${aws_s3_bucket.test.bucket_domain_name}"
 }
--- terraform/demo2/terraform.tfvars
+++ terraform/demo2/terraform.tfvars
@@ -7,4 +7,8 @@ iam_users = [
   "circleci",
   "user1",
   "user2",
+  "user3",
+  "user4",
+  "user5",
+  "user6",
 ]
